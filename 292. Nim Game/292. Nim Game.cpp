879c95ebd8b53dbd41c3b1ab20d184bc88240690
Himanshu Singh <avrockhimanshu007@gmail.com>
2025-05-09 01:18:13 +0530

    Fifth Question Leetcode

---
+++ 292. Nim Game/292. Nim Game.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool canWinNim(int n)
+    {
+        if (n % 4 != 0)
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+};
